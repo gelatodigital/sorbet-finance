2c55be7c27f53d8a97e038a3e7fefd1878eb5577
Hilmar X <x@hilmarx.com>
2021-06-01 07:20:46 +0200

    removed suprise

--- src/pages/Swap/index.js
+++ src/pages/Swap/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import ExchangePage from '../../components/ExchangePage'
 import Orders from '../../components/Orders'
-import SurpriseModal from '../../components/Surprise/SurpriseModal'
 
 export default function Swap({ initialCurrency }) {
   return (
     <>
-      <SurpriseModal />
       <ExchangePage initialCurrency={initialCurrency} />
       <Orders />
     </>
